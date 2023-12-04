b3dda3a36bfa8bc4dd21caf4859e5f7811d097bb
Grant Kurtz <th3guys2@gmail.com>
2023-12-04 10:19:21 -0500

    Day 4 puzzle 2 working

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,8 +1,9 @@
 package day4
 
 import java.io.File
-import java.lang.Math.pow
 import kotlin.math.pow
+
+data class Card(val wins: Int, var copies: Int)
 
 fun main(_args: Array<String>) {
 
@@ -12,19 +13,41 @@ fun main(_args: Array<String>) {
     println("Puzzle 1: ${sumWinningMatches("input/day4/input")}")
 
     /**
-     * Solution:
+     * Solution: 8063216
      */
-//    println("Puzzle 2: ${findGearRatios("input/day4/input")}")
+    println("Puzzle 2: ${playInsaneScratchCardGame("input/day4/input")}")
 }
 
 fun sumWinningMatches(filename: String): Int {
     var total = 0
     File(filename).forEachLine { line ->
-        val data = line.split(":  ", ": ").last().split(" |  ", " | ")
-        val winning = data.first().split("  ", " ").map { s -> s.toInt() }.sorted().toSet()
-        val ours = data.last().split("  ", " ").map { s -> s.toInt() }.sorted().toSet()
+        val (winning, ours) = processGame(line)
         total += 2.0.pow(ours.intersect(winning).size.toDouble() - 1.0).toInt()
     }
 
     return total
+}
+
+fun playInsaneScratchCardGame(filename: String): Int {
+    val games = mutableListOf<Card>()
+    File(filename).forEachLine { line ->
+        val (winning, ours) = processGame(line)
+        games.add(Card(ours.intersect(winning).size, 0))
+    }
+
+    games.forEachIndexed { index, card ->
+        for (w in 1..card.wins) {
+            if (w + index < 202)
+                games[w + index].copies += card.copies + 1
+        }
+    }
+
+    return games.sumOf { card -> card.copies } + games.size
+}
+
+fun processGame(line: String): Pair<Set<Int>, Set<Int>> {
+    val data = line.split(":  ", ": ").last().split(" |  ", " | ")
+    val winning = data.first().split("  ", " ").map { s -> s.toInt() }.sorted().toSet()
+    val ours = data.last().split("  ", " ").map { s -> s.toInt() }.sorted().toSet()
+    return Pair(winning, ours)
 }
