31125e27ba38ef4936fa4a586e66c881aa301d4f
Grant Kurtz <th3guys2@gmail.com>
2023-12-05 20:56:31 -0500

    refactored Day 5 puzzle 2

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -26,15 +26,14 @@ fun main(_args: Array<String>) {
 fun findLowestLocationAmongRanges(filname: String): Long {
     var state = "initial"
     var followedSeeds = mutableListOf<SeedRange>()
-    File(filname).forEachLine read@ { line ->
+    File(filname).forEachLine read@{ line ->
         if (line.isEmpty())
             return@read
-        when(state) {
+        when (state) {
             "initial" -> { // first line of seeds
                 if (line.contains("map")) {
                     state = "ranges"
-                }
-                else {
+                } else {
                     followedSeeds.addAll(line.split(": ").last().split(" ").chunked(2).map { (s, r) ->
                         val start = s.toLong()
                         val range = r.toLong() - 1
@@ -42,39 +41,46 @@ fun findLowestLocationAmongRanges(filname: String): Long {
                     })
                 }
             }
+
             "ranges" -> { // seed to soil map section
                 if (line.contains("map")) {
                     followedSeeds.forEach { it.moved = false }
-                }
-                else {
+                } else {
                     // DST IS FIRST, SRC IS SECOND
                     val (dst, src) = processRanges(line)
                     val processed = followedSeeds.filter { it.moved }.toMutableList()
                     followedSeeds.retainAll { !it.moved }
-                    while(followedSeeds.isNotEmpty()) {
+                    while (followedSeeds.isNotEmpty()) {
                         val s = followedSeeds.removeFirst()
                         val seedRange = s.range
                         if (seedRange.start in src && seedRange.endInclusive in src) {
                             adjustRange(s, dst, src)
                             processed.add(s)
-                        }
-                        else if (seedRange.start in src) {
-                            val unmoved = LongRange(src.endInclusive + 1, seedRange.endInclusive)
-                            val moved = LongRange(seedRange.start, src.endInclusive)
-                            followedSeeds.add(SeedRange(unmoved, false))
-                            val newSeedRange = SeedRange(moved, true)
-                            processed.add(newSeedRange)
-                            adjustRange(newSeedRange, dst, src)
-                        }
-                        else if (seedRange.endInclusive in src) {
-                            val unmoved = LongRange(seedRange.start, src.start - 1)
-                            val moved = LongRange(src.start, seedRange.endInclusive)
-                            followedSeeds.add(SeedRange(unmoved, false))
-                            val newSeedRange = SeedRange(moved, true)
-                            processed.add(newSeedRange)
-                            adjustRange(newSeedRange, dst, src)
-                        }
-                        else {
+                        } else if (src.start in seedRange && src.endInclusive in seedRange) {
+                            breakupRanges(
+                                seedRange.start, src.start - 1,
+                                src.start, src.endInclusive,
+                                src.endInclusive + 1, seedRange.endInclusive,
+                                dst, src,
+                                followedSeeds, processed
+                            )
+                        } else if (seedRange.start in src) {
+                            breakupRanges(
+                                0, -1,
+                                seedRange.start, src.endInclusive,
+                                src.endInclusive + 1, seedRange.endInclusive,
+                                dst, src,
+                                followedSeeds, processed
+                            )
+                        } else if (seedRange.endInclusive in src) {
+                            breakupRanges(
+                                seedRange.start, src.start - 1,
+                                src.start, seedRange.endInclusive,
+                                0, -1,
+                                dst, src,
+                                followedSeeds, processed
+                            )
+                        } else {
                             processed.add(s)
                         }
                     }
@@ -87,6 +93,35 @@ fun findLowestLocationAmongRanges(filname: String): Long {
 
     return followedSeeds.minOf { s -> s.range.start }
 }
+
+fun breakupRanges(
+    lowerStart: Long, lowerEnd: Long,
+    movedStart: Long, movedEnd: Long,
+    upperStart: Long, upperEnd: Long,
+    dst: LongRange, src: LongRange,
+    followed: MutableList<SeedRange>, processed: MutableList<SeedRange>
+) {
+    if (lowerStart < lowerEnd) { // if false, they were equal or smaller
+        val unmovedLower = LongRange(lowerStart, lowerEnd)
+        assertValidRange(unmovedLower)
+        followed.add(SeedRange(unmovedLower, false))
+    }
+
+    val moved = LongRange(movedStart, movedEnd)
+    assertValidRange(moved)
+
+    if (upperStart < upperEnd) { // if equal, they were equal or smaller
+        val unmovedHigher = LongRange(upperStart, upperEnd)
+        assertValidRange(unmovedHigher)
+        followed.add(SeedRange(unmovedHigher, false))
+    }
+
+    val newSeedRange = SeedRange(moved, true)
+    processed.add(newSeedRange)
+    adjustRange(newSeedRange, dst, src)
+    assertValidRange(moved)
+}
+
 fun adjustRange(s: SeedRange, dst: LongRange, src: LongRange) {
     val seedRange = s.range
     val start = seedRange.start
@@ -99,25 +134,24 @@ fun adjustRange(s: SeedRange, dst: LongRange, src: LongRange) {
 fun findLowestLocation(filname: String): Long {
     var state = "initial"
     lateinit var followedSeeds: List<Seed>
-    File(filname).forEachLine read@ { line ->
+    File(filname).forEachLine read@{ line ->
         if (line.isEmpty())
             return@read
-        when(state) {
+        when (state) {
             "initial" -> { // first line of seeds
                 if (line.contains("map")) {
                     state = "ranges"
-                }
-                else {
+                } else {
                     followedSeeds = line.split(": ").last().split(" ").map { s ->
                         Seed(s.toLong(), false)
                     }
                 }
             }
+
             "ranges" -> { // seed to soil map section
                 if (line.contains("map")) {
                     followedSeeds.forEach { it.moved = false }
-                }
-                else {
+                } else {
                     // DST IS FIRST, SRC IS SECOND
                     val (dst, src) = processRanges(line)
                     followedSeeds.filter { s -> !s.moved }.forEach { s ->
@@ -143,4 +177,10 @@ fun findLowestLocation(filname: String): Long {
 fun processRanges(line: String): Pair<LongRange, LongRange> {
     val (dst, src, rng) = line.split(" ").map { it.toLong() }
     return Pair(LongRange(dst, dst + rng - 1), LongRange(src, src + rng - 1))
+}
+
+fun assertValidRange(range: LongRange) {
+    assert(range.start <= range.endInclusive) {
+        "${range.start} is not less than ${range.endInclusive}"
+    }
 }
